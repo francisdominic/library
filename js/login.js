52b6fa44638e0a619134a7ee316459290712b7d0
francis <fmanibad@gmail.com>
2024-11-26 00:43:07 +0800

    add otp

--- js/login.js
+++ js/login.js
@@ -16,6 +16,17 @@ function logIn() {
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
+      // ask and check the provided OTP before redirecting to admin dashboard
+      const secret = "FR5WVXRLBY5ZZJEX";
+      const otplib = window.otplib;
+      do {
+        var otp = prompt("Enter OTP: ");
+        var isValid = otplib.authenticator.check(otp, secret); // Verify OTP
+        if (!isValid) {
+          alert("OTP is invalid! Please try again.");
+        }
+      } while (!isValid);
+
       window.location.href = "/admin/dashboard.html";
     })
     .catch((error) => {
@@ -27,9 +38,4 @@ function logIn() {
 }
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("login-btn").addEventListener("click", logIn);
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      window.location.href = "/admin/dashboard.html";
-    }
-  });
 });
